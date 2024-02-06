3d3577a94f3550ef4ec6bad3cecc20f8aa6e9072
XuFang <157049028+XuFangGG@users.noreply.github.com>
2024-02-06 17:44:24 +0800

    SkyBlockCore
    SkyBlockCore
    version:0.0.1

---
+++ SkyBlock/src/main/java/club/blocklife/skyblock/SkyBlock.java
@@ -0,0 +1,17 @@
+package club.blocklife.skyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
